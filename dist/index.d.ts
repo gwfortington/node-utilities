07412127a6028002380457a26b45bea7392cc7c7
Grant Fortington <gwfortington@hotmail.com>
2024-08-10 12:57:01 +0930

    build

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare const objectsEqual: (object1: Record<string, any>, object2: Record<string, any>) => boolean;
+export declare const pick: (object: Record<string, any>, keys: string[]) => Record<string, any>;
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,32 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pick = exports.objectsEqual = void 0;
+const objectsEqual = (object1, object2) => {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+    let value1;
+    let value2;
+    for (const key of keys1) {
+        value1 = object1[key];
+        value2 = object2[key];
+        if (value1 instanceof Date) {
+            value1 = value1.getDate();
+            value2 = value2.getDate();
+        }
+        if (value1 !== value2) {
+            return false;
+        }
+    }
+    return true;
+};
+exports.objectsEqual = objectsEqual;
+const pick = (object, keys) => {
+    return keys.reduce((newObject, key) => {
+        newObject[key] = object[key];
+        return newObject;
+    }, {});
+};
+exports.pick = pick;
